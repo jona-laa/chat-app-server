713693eb12f6d8cb862417368b8ff7df8b6258f4
Jonathan Laasonen <iphntm@iPHNTM.local>
2020-01-19 21:43:38 +0100

    basic morgan logging setup

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const app = require('express')();
 const http = require('http').Server(app);
-const logger = require('./middleware/logger');
 const io = require('socket.io')(http);
-const port = 4200;
 const cors = require('cors');
 var users = require('./routes/users');
 require('./chat.js')(io);
+const morgan = require('./middleware/morgan');
+const logger = require('./middleware/logger');
 
 app.use(cors());
+app.use(morgan);
 app.use(logger);
 app.use('/users', users);
 
+const port = 4200;
 http.listen(port, () => console.log('Server running on port ', port));
